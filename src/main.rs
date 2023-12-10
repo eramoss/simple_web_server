2d40ee1ff0b208b34058320f7a280e93b5a36e21
eramoss <edulramos@outlook.com>
2023-12-09 21:13:08 -0300

    simple response

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,23 @@ fn handle_connection(stream: &TcpStream) -> Result<HttpRequest, ()> {
     Ok(request)
 }
 
+fn send_response(buf: &[u8], mut stream: &TcpStream) -> std::io::Result<()> {
+    stream
+        .write_all(buf)
+        .expect(format!("cannot write on stream {:#?}", stream).as_str());
+
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
-    let listener =
-        TcpListener::bind(("127.0.0.1", 8080)).expect("unable to bind in localhost:8080");
+    let listener = TcpListener::bind(("127.0.0.1", 8080))?;
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connected to stream '{:?}'", stream);
+        let stream = stream?;
         let request =
             handle_connection(&stream).expect("unable to handle connection to read stream");
 
+        send_response(b"hello warudo", &stream)?;
         println!("{}", request.value());
     }
 
