a0380559019b92183e6b06061fdb6e8f80c4aa57
eramoss <edulramos@outlook.com>
2023-12-16 15:43:15 -0300

    make it more simple

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use simple_web_server::http_request::HttpRequest;
+use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_connection(stream: &TcpStream) -> Result<HttpRequest, ()> {
+fn handle_connection(stream: &TcpStream) -> Result<String, ()> {
     let buffer_read = BufReader::new(stream);
-    let request: HttpRequest = buffer_read
+    let request = buffer_read
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
@@ -12,24 +12,21 @@ fn handle_connection(stream: &TcpStream) -> Result<HttpRequest, ()> {
     Ok(request)
 }
 
-fn send_response(buf: &[u8], mut stream: &TcpStream) -> std::io::Result<()> {
-    stream
-        .write_all(buf)
-        .expect(format!("cannot write on stream {:#?}", stream).as_str());
-
-    Ok(())
-}
-
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind(("127.0.0.1", 8080))?;
+    let listener = TcpListener::bind(("0.0.0.0", 8080))?;
 
     for stream in listener.incoming() {
-        let stream = stream?;
-        let request =
+        let mut stream = stream?;
+        let _request =
             handle_connection(&stream).expect("unable to handle connection to read stream");
 
-        send_response(b"hello warudo", &stream)?;
-        println!("{}", request.value());
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 
     Ok(())
