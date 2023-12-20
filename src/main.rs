641391301b18397c1e4bde04a87868e8f994cff4
eramoss <edulramos@outlook.com>
2023-12-20 13:21:05 -0300

    added 404

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,29 @@ use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_connection(stream: &TcpStream) -> Result<String, ()> {
-    let buffer_read = BufReader::new(stream);
-    let request = buffer_read
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    Ok(request)
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind(("0.0.0.0", 8080))?;
 
     for stream in listener.incoming() {
-        let mut stream = stream?;
-        let _request =
-            handle_connection(&stream).expect("unable to handle connection to read stream");
-
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+        let stream = stream?;
+        let _request = handle_connection(stream);
     }
 
     Ok(())
